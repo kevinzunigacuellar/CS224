98d2b07f67b82f138eabed808ffddf48dc19acdc
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-04-13 20:53:58 -0400

    add file read

--- Knapsack/Knapsack.java
+++ Knapsack/Knapsack.java
@@ -1,5 +1,5 @@
-package Knapsack;
 
+import java.io.*;
 import java.util.*;
 
 class Knapsack {
@@ -9,15 +9,18 @@ class Knapsack {
     int i, w;
     int M[][] = new int[n + 1][W + 1];
 
-    // sort by weight before starting
-
     // fill M with -1
     for (int[] row : M) {
       Arrays.fill(row, -1);
     }
 
+    // print header
+    System.out.println("Solving knapsack weight capacity " + W + ", with " + n + " items");
+    System.out.println("============================================================");
+
+    // knapsack algorithm bottom up
     for (i = 0; i <= n; i++) {
-      System.out.println("Memoization table, Row " + i + " completed");
+      System.out.println("Memoization table, Row " + i + " completed \n");
       for (w = 0; w <= W; w++) {
         if (i == 0 || w == 0)
           M[i][w] = 0;
@@ -28,55 +31,90 @@ class Knapsack {
       }
       print_table(M, n, W);
     }
-    System.out.println("Knapsack with weight capacity " + W + " has optimal value: " + M[n][W]);
-    System.out.println("___Knapsack Contains___");
-    while (n != 0) {
-      if (M[n][W] != M[n - 1][W]) {
-        System.out.println("Item " + n + " (Value = " + vi[n - 1] + " Weight = " + wi[n - 1] + ")");
-        W = W - wi[n - 1];
-      }
-
-      n--;
-    }
+    // return items that are in the knapsack
+    print_items(M, n, W, wi, vi);
 
     return M[n][W];
   }
 
-  // helper function to print memoization table
-  static void print_table(int M[][], int n, int W) {
-    System.out.println("0 1 2 3 4 5 6");
-    System.out.println("-------------------");
-    for (int r = 0; r <= n; r++) {
-      System.out.print("{");
-      for (int i = 0; i <= r; i++) {
-        System.out.print(" ");
-        if (i == 0) {
-          continue;
-        } else {
-          System.out.print(i);
-          System.out.print(" ");
-        }
+  // helper method to print the items in the knapsack
+  static void print_items(int M[][], int n, int W, int wi[], int vi[]) {
+    System.out.println("Knapsack with weight capacity " + W + " has optimal value: " + M[n][W] + "\n");
+    System.out.println("________Knapsack Contains________");
+    while (n != 0) {
+      if (M[n][W] != M[n - 1][W]) {
+        System.out.printf("Item " + n + " (Value = %2d, Weight = %2d) \n", vi[n - 1], wi[n - 1]);
+        W = W - wi[n - 1];
       }
-      System.out.print("} ");
-      ;
-      for (int c = 0; c <= W; c++) {
-        System.out.print(M[r][c] + " ");
-      }
-      System.out.println();
+      n--;
     }
   }
 
-  // helper method that returns the max value
+  // helper function to print memoization table
+  static void print_table(int M[][], int n, int W) {
+    int arr[] = new int[W + 1];
+    System.out.printf("%10s", " ");
+    for (int i = 0; i <= W; i++) {
+      arr[i] = i;
+      System.out.printf("%5d", arr[i]);
+    }
+    System.out.println();
+    System.out.println("-----------------------------------------------------");
+    for (int r = 0; r <= n; r++) {
+      String strg = "{";
+      for (int i = 0; i <= r; i++) {
+        if (i == 0) {
+          continue;
+        } else if (i == r) {
+          strg += i;
+        } else {
+          strg += i + ",";
+        }
+      }
+      strg = strg + "}";
+      System.out.printf("%7s | ", strg);
+
+      for (int c = 0; c <= W; c++) {
+        System.out.printf("%5d", M[r][c]);
+      }
+      System.out.println();
+    }
+    System.out.println("\n \n");
+
+  }
+
+  // helper function that returns the max value
   static int max(int a, int b) {
     return (a > b) ? a : b;
   }
 
   // Main function
   public static void main(String args[]) {
-    int vi[] = new int[] { 6, 18, 2 };
-    int wi[] = new int[] { 5, 2, 1 };
-    int W = 6;
+    // initialize variables
+    int vi[] = new int[3];
+    int wi[] = new int[3];
+    int W;
     int n = vi.length;
-    knapSack(W, wi, vi, n);
+
+    // read file
+    try {
+      File myInput = new File("sample_input.txt");
+      Scanner s = new Scanner(myInput);
+      W = s.nextInt();
+      while (s.hasNextInt()) {
+        int index = s.nextInt();
+        vi[index - 1] = s.nextInt();
+        wi[index - 1] = s.nextInt();
+      }
+      s.close();
+
+      // call knapsack algorithm
+      knapSack(W, wi, vi, n);
+
+      // catch exceptions
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
   }
 }
